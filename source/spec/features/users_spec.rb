7ecdd0387bbec29310746e312675e539aa46de91
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-18 10:21:54 -0400

    Structure of tests completed with pendings

--- source/spec/features/users_spec.rb
+++ source/spec/features/users_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 require 'rails_helper'
 
-feature 'ability to access home page' do 
+feature 'ability to access home page' do
 
   scenario 'user visits root path' do
   	visit root_path
   end
 
 end
+
+feature 'ability to sign up as a user' do
+
+  scenario 'user fills out sign up form without errors' do
+    pending
+  end
+
+  scenario 'user fills out sign up form with errors' do
+    pending
+  end
+
+end
+
+feature 'ability to sign in as an existing user' do
+
+  scenario 'user fills out sign in form without errors' do
+    pending
+  end
+
+  scenario 'user fills out sign in form with errors' do
+    pending
+  end
+
+end
+
+
