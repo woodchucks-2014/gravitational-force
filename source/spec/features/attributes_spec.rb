9c591f71289fb552da89751d08480b1d7dbe3cad
Greg <knudsegi@hotmail.com>
2014-07-18 16:12:28 -0400

    remove 'require spec_helper' from feature test pages

--- source/spec/features/attributes_spec.rb
+++ source/spec/features/attributes_spec.rb
@@ -1,7 +1,6 @@
-require 'spec_helper'
 require 'rails_helper'
 
-feature 'ability to view attributes' do 
+feature 'ability to view attributes' do
 
   scenario 'when user visits index' do
     FactoryGirl.create :attribute
