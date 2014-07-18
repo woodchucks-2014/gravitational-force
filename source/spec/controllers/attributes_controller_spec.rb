21ebe1b7fb460a569d50912417f1838eb73687ee
Drew Teter <mteter13@gmail.com>
2014-07-18 11:45:42 -0400

    First four tests passing
    You can now visit the attributes#index page and see each attribute's name.

--- source/spec/controllers/attributes_controller_spec.rb
+++ source/spec/controllers/attributes_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 
 describe AttributesController do
   it "assigns all attributes in index" do
+    FactoryGirl.create :attribute
     get :index
     expect(assigns(:attributes)).to eq Attribute.all
   end
--- source/spec/features/attributes_spec.rb
+++ source/spec/features/attributes_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 feature 'ability to view attributes' do 
 
   scenario 'when user visits index' do
-    visit attributes_path
     FactoryGirl.create :attribute
+    visit attributes_path
     expect(page).to have_content "Attribute_name"
   end
 
