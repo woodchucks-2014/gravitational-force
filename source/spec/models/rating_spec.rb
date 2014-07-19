ad162ddf692510f366e427f30b9896b0f5d33837
& <pair+benbrostoff.scoin@devbootcamp.com>
2014-07-18 22:27:36 -0400

    Added additional tests on ratings

--- source/spec/models/rating_spec.rb
+++ source/spec/models/rating_spec.rb
@@ -7,4 +7,16 @@ describe Rating do
     expect(rating).to be_instance_of(Rating)
   end
 
+  it "should belong to trait" do
+    expect(rating).to respond_to :trait
+  end
+
+  it "should belong to a rated user" do
+    expect(rating).to respond_to :rated_user
+  end
+
+  it "should belong to a rating user" do
+    expect(rating).to respond_to :rating_user
+  end
+
 end
