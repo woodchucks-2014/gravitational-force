ef04318b5701bae45d29bf7ed170d9363e275870
Ben Brostoff & Michael Drew Teter <pair+benbrostoff.mdrewt@devbootcamp.com>
2014-07-19 16:52:15 -0400

    rewrote our ratings controller test into two tests, made a ratings factory, added code to pass test

--- source/spec/controllers/ratings_controller_spec.rb
+++ source/spec/controllers/ratings_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe RatingsController do
-  it "prepares all ratings in index" do
-    FactoryGirl.create :trait
+  it "prepares existing rating in index" do
+    rating = FactoryGirl.create :rating
+    trait = FactoryGirl.create :trait
     user = FactoryGirl.create :user
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     get :index, user_id: user.id
-    # expect(assigns(:ratings)).to eq Trait.all
+    expect(assigns(:ratings).first).to eq(rating)
   end
+
+  # it "prepares new ratings in index" do
+  #   pending
+  # end
 end
