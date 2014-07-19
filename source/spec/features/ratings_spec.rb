ef47be43ad0d6650005184fb3da343d73a5e13ae
Ben Brostoff & Michael Drew Teter <pair+benbrostoff.mdrewt@devbootcamp.com>
2014-07-19 17:07:33 -0400

    the errors have mostly been taken care of

--- source/spec/features/ratings_spec.rb
+++ source/spec/features/ratings_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 feature 'ability to view ratings' do
 
-  scenario 'when user visits index' do
+  scenario 'when user visits index, trait name appears on form' do
     user = FactoryGirl.create :user
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
@@ -12,15 +12,15 @@ feature 'ability to view ratings' do
     expect(page).to have_content t.name.capitalize
   end
 
-  scenario 'when user rates a trait' do
+  scenario 'when user rates a trait, a new rating is created' do
     user = FactoryGirl.create :user
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
     trait = FactoryGirl.create :trait
-    visit user_traits_path(user)
+    visit user_ratings_path(user)
     find(:xpath, "//input[@id='#{trait.name}_rating']").set 57
     check('something')
-    click_button('Update Trait')
+    click_button('Update User')
     expect(Rating.all).to change(Rating, :count).by 1
   end
 
