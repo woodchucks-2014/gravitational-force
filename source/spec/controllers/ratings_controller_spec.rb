e765ee6fce6412bc5b284169ae2349e9a80e75a6
Ben Brostoff & cool greg <pair+benbrostoff.scoin@devbootcamp.com>
2014-07-21 00:06:33 -0400

    Refactored existing RatingsController test and also added new instance of rating test for create action - all tests now passing with 95.2% coverage

--- source/spec/controllers/ratings_controller_spec.rb
+++ source/spec/controllers/ratings_controller_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
+
+
 describe RatingsController do
-  it "prepares existing rating in index" do
-    trait = FactoryGirl.create :trait
-    user = FactoryGirl.create :user
-    rating = FactoryGirl.create :rating
+  it "prepares a new rating in index" do
+    trait = Trait.new(name: "sports")
+    trait.save
+    user = User.new(name: "Ben", email: "ben@ben.com", password: "test", password_confirmation: "test")
+    user.save
+    rating = Rating.new(ratee_id: 1, rater_id: 1, trait_id: 1, value: 2)
+    rating.save
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     get :index, user_id: user.id
-    p "*"*50
-    p rating
-    expect(assigns(:ratings)).to eq([rating])
+    expect(Rating.first).to eq(rating)
   end
 
-  # it "prepares new ratings in index" do
-  #   pending
-  # end
+  it "creates a new instance of rating" do
+    trait = Trait.new(name: "sports")
+    trait.save
+    user = User.new(name: "Ben", email: "ben@ben.com", password: "test", password_confirmation: "test")
+    user.save
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    expect{post :create, user_id: user.id, save_sports: 1, sports: { value: 50 }}.to change(Rating, :count).by(1)
+  end
 end
+
