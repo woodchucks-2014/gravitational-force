76d36612567a66d5f3d1d08d917a010fa1d01182
Ben Brostoff & cool greg <pair+benbrostoff.scoin@devbootcamp.com>
2014-07-20 20:32:13 -0400

    Made changes in ratings controller and view

--- source/app/controllers/ratings_controller.rb
+++ source/app/controllers/ratings_controller.rb
@@ -4,8 +4,8 @@ class RatingsController < ApplicationController
     @user = User.find(params[:user_id])
     @ratings = []
     Trait.all.each do |trait|
-      rating = Rating.where(rated_user_id: @user.id, trait_id: trait.id).first
-      rating ||= Rating.new(rated_user_id: @user.id, rating_user_id: current_user.id, trait_id: trait.id)
+      rating = Rating.where(ratee_id: @user.id, trait_id: trait.id).first
+      rating ||= Rating.new(ratee_id: @user.id, rater_id: current_user.id, trait_id: trait.id)
       @ratings << rating
     end
   end
